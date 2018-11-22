12f662857f0ff8ed71a5261adff7057bba25bf22
Student226123 <Kacper2345!>
2018-11-22 16:42:14 +0100

    readme and comments

---
+++ README.md
@@ -0,0 +1,39 @@
+<h2>Weird Text</h2><br>
+Task:<br>
+<h3>WeirdText encoding and decoding</h3>
+WeirdText is a text encoding.<br>
+It is not "encryption" because humans can usually read it quite easily.  But
+machines may find it difficult to read without the list of original words.  Except
+of having fun, there are real-world applications for this, e.g. if encryption is
+forbidden by law in your country, but you still don't want your email content to
+get automatically processed somehow.<br>
+<h3>Encoding</h3>
+For each original word in the original text, leave the first and last character of it
+in that position, but shuffle (permutate) all the characters in the middle of the
+word.  If possible, the resulting "encoded" word MUST NOT be the same as the
+original word. Keep everything else (whitespace, punctuation, etc.) like in the
+original.  To make decoding by a machine possible, your encoder shall also
+output a sorted list of original words (only include words that got shuffled, not
+text that did not).<br>
+The composite output of the encoder (see example below) contains encoded
+text (WeirdText) and also the sorted list of original words.
+<h3>Decoding</h3>
+For decoding composite text, first do a simple check whether the text looks like
+composite output of your encoder. If not, raise some reasonable exception.
+Then, use the encoded text and the words list to decode the text.
+Your decoded output should, as far as possible, be identical to the original
+text. In case of ambiguities (some encoded word could have been multiple
+original words), decoding errors are acceptable.
+<h3>Example</h3>
+Original Text (this is a single string formatted nicely for better viewing!)::<br>
+'This is a long looong test sentence,\n'<br>
+'with some big (biiiiig) words!'<br>
+Encoded Text (see comment above):<br>
+'\n---weird---\n'<br>
+'Tihs is a lnog loonog tset sntceene,\n'<br>
+'wtih smoe big (biiiiig) wdros!'<br>
+'\n---weird---\n'<br>
+'long looong sentence some test This with words'<br>
+Decoded Text::<br>
+'This is a long looong test sentence,\n'<br>
+'with some big (biiiiig) words!'<br>
--- weird_text.py
+++ weird_text.py
@@ -4,10 +4,13 @@ from random import shuffle
 
 
 def encode(text):
+    """Function to encode text"""
+    # spliting sentence to list of words
     listed_words = text.split()
     encoded_sentence_word_list = []
     unshuffled_sorted_words = []
     for word in listed_words:
+        # excluding words with 3 or less letters
         if len(word) <= 3:
             encoded_sentence_word_list.append(word)
         else:
@@ -15,6 +18,7 @@ def encode(text):
             prefix = ""
             curr_char = ""
             counter_prefix = 0
+            # this loop handles prefix for out word, starts from left, ends when finds first numeric char
             while not re.match("^[a-zA-Z0-9_]*$", curr_char) or curr_char == "":
                 curr_char = listed_chars[counter_prefix]
                 prefix += curr_char
@@ -22,32 +26,45 @@ def encode(text):
             suffix = ""
             curr_char = ""
             counter_suffix = len(listed_chars) - 1
+            # this loop handles suffix for out word, starts from right, ends when finds first numeric char
             while not re.match("^[a-zA-Z0-9_]*$", curr_char) or curr_char == "":
                 curr_char = listed_chars[counter_suffix]
                 suffix += curr_char
                 counter_suffix -= 1
+            # reverse suffix
             suffix = suffix[::-1]
+            # taking the middle of word to shuffle it. Using prefix and suffix indexes
             word_middle = [let for i, let in enumerate(listed_chars) if counter_prefix <= i <= counter_suffix]
             shuffled_word = copy(word_middle)
+            # handles saving our key words
             if not ''.join(word_middle) == len(word_middle) * word_middle[0]:
+                # pre and suf are suffixes without special signs
                 pre = ''.join(char for char in prefix if char.isalnum())
                 suf = ''.join(char for char in suffix if char.isalnum())
+                # saving unshuffled word before shuffle
                 unshuffled_sorted_words.append(pre + "".join(word_middle) + suf)
+                # distinct words wanted, no need for copies
                 unshuffled_sorted_words = set(unshuffled_sorted_words)
                 unshuffled_sorted_words = list(unshuffled_sorted_words)
+            # shuffle middle word while different and all letters not the same
             while shuffled_word == word_middle and not ''.join(word_middle) == len(word_middle) * word_middle[0]:
                 shuffle(shuffled_word)
+            # put encoded word together
             encoded_word_temp = prefix + ''.join(shuffled_word) + suffix
             encoded_sentence_word_list.append(encoded_word_temp)
+    # sorting string list and ignoring case
     unshuffled_sorted_words = sorted(unshuffled_sorted_words, key=lambda s: s.lower())
     return " ".join(encoded_sentence_word_list), unshuffled_sorted_words
 
 
 def decode(text, sorted_words_list):
+    """Function to decode text"""
     listed_words = text.split()
     decoded_sentence_word_list = []
+    # get middle from key words
     middle_sorted_words_list = [word[1:-1] for word in sorted_words_list]
     for word in listed_words:
+        # half the same as encode
         if len(word) <= 3:
             decoded_sentence_word_list.append(word)
         else:
@@ -69,22 +86,30 @@ def decode(text, sorted_words_list):
             suffix = suffix[::-1]
             word_middle = [let for i, let in enumerate(listed_chars) if counter_prefix <= i <= counter_suffix]
             word_found = False
+            # crating list with only key words the same size as encoded word
             same_size_list = [s for s in middle_sorted_words_list if len(s) == len(word_middle)]
+            # this variable will save our decoded word
             wanted_word = ""
+            # do until word not with same letters and word not found yet
             while not ''.join(word_middle) == len(word_middle) * word_middle[0] and not word_found:
                 for s in same_size_list:
-                    char_list = list(s)
                     count_good = 0
                     wanted_word = ""
-                    for c in char_list:
+                    # for every char in middle word key
+                    for c in s:
+                        # for every char in encoded word
                         for x in word_middle:
                             if x == c:
                                 count_good += 1
+                                # creating word if chars the same
                                 wanted_word += c
+                                # no need to iterate more if found
                                 break
+                    # if word length matches, no need to iterate more - word found
                     if count_good == len(word_middle):
+                        word_found = True
                         break
-                word_found = True
+            # put decoded word together
             decoded_word_temp = prefix + wanted_word + suffix
             decoded_sentence_word_list.append(decoded_word_temp)
 
